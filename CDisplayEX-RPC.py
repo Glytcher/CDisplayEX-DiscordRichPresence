f9f1820dcfa90f9eeadd477e5bf1dbf1271f67ea
Glytcher <28837783+Glytcher@users.noreply.github.com>
2022-08-19 11:24:52 +0200

    Removed the file extension in rpc using regex And some general spelling and cleanup

--- CDisplayEX-RPC.py
+++ CDisplayEX-RPC.py
@@ -1,17 +1,17 @@
 from pypresence import Presence
-import time, json, os, sys, psutil, win32gui, win32process
+import time, json, os, sys, psutil, win32gui, win32process, re
 
 # Load in config
 me = os.path.dirname(os.path.abspath(__file__))     
 try:
-    config = json.load(open(me + "/config.json", 'r'))
+    config = json.load(open(me + "/config.json", "r"))
     print("Successfully loaded config.json")
     print("\n\nConfig file reads:\n" + str(config))
 except Exception as e:
     print("config.json does not exist.")
     print("Error\n" + str(e))
 
-# Check if a process is running or not, parameter is the name of the process, will be used to check in Cdisplay is running
+# Check if a process is running or not, parameter is the name of the process, will be used to check in CDisplayEx is running
 def isProcessRunning(processName):
     for proc in psutil.process_iter():
         try:
@@ -33,13 +33,13 @@ def getWindowHandle(pid):
   win32gui.EnumWindows (callback, hwnds)
   return hwnds
 
-# Get Cdisplay PID
+# Get CDisplayEx PID
 def findProcessIdByName(processName):
     listOfProcessObjects = []
     for proc in psutil.process_iter():
        try:
-           pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
-           if processName.lower() in pinfo['name'].lower() :
+           pinfo = proc.as_dict(attrs=["pid", "name", "create_time"])
+           if processName.lower() in pinfo["name"].lower() :
                listOfProcessObjects.append(pinfo)
        except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
            pass
@@ -51,7 +51,7 @@ def getCdisplayID():
 
 # Get window tile of CDisplayEX from PID using get_hwnds_for_pid()
 def getManga():
-    if isProcessRunning('Cdisplayex'):
+    if isProcessRunning("Cdisplayex"):
         for hwnd in getWindowHandle(getCdisplayID()):
             title = str(win32gui.GetWindowText(hwnd))
             if title.lower() == "cdisplayex" or title == None:
@@ -59,10 +59,11 @@ def getManga():
             if "+" in title.lower(): # When reading in double page mode, only show first page to make it look neater
                 title = title.split("+", 1)[0]
                 title = title.replace("[", " ", 1)
-            title = title[:125] + (title[125:] and "...") # Discord RPC can't have more then 128 characters so we cut off at 50 and add ...
+            title = re.sub(r'\.(jpg|jpeg|png|webp)', "", title, flags=re.IGNORECASE) # Remove image file extensions
+            title = title[:125] + (title[125:] and "...") # Discord RPC can"t have more then 128 characters so we cut off at 50 and add three dots
             return title
     else:
-        print('No Cdisplayex process is running')
+        print("No CDisplayEx process is running")
         return "CDisplayEX is not running"
 
 # Discord Rich Presence
@@ -74,7 +75,7 @@ startTime = time.time()
 
 def main():
     while True:
-        while isProcessRunning('Cdisplayex'):
+        while isProcessRunning("Cdisplayex"):
             try:
                 RPC.update(
                         large_image=config["images"]["largeImage"], 
@@ -92,18 +93,18 @@ def main():
                 break
         else:
             if not findProcessIdByName("cdisplayex"):
-                print("Cannot find CDisplayEX...")
+                print("Cannot find CDisplayEx...")
                 time.sleep(int(config["time"]["closeTime"]))
                 while not isProcessRunning("Cdisplayex"):
-                    print("No connection...\nCdisplayex will close")
+                    print("No connection...\nCDisplayExx will close")
                     RPC.close()
                     sys.exit(1)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
